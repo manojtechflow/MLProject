a1c19c9c31614fdc0641499640f6a4414e894fd0
manojtechflow <mkthota1112@gmail.com>
2024-03-03 07:06:50 +0000

    prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -8,7 +8,7 @@ from src.logger import logging
 import dill
 
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -23,15 +23,20 @@ def save_object(file_path, obj):
         logging.error(CustomException(e,sys))
         raise CustomException(e, sys)
     
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for model_name, model_instance in models.items():
 
+            para = params[model_name]
             model_instance.fit(X_train, y_train)
+            gs = GridSearchCV(model_instance, para, cv=3) 
+            gs.fit(X_train, y_train)
 
-            y_test_pred = model_instance.predict(X_test)
+            best_params = gs.best_params_
+
+            y_test_pred = gs.predict(X_test)
 
             test_model_score = r2_score(y_test, y_test_pred)
 
@@ -41,3 +46,11 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
     except Exception as e:
         logging.error(CustomException(e, sys))
         raise CustomException(e, sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
